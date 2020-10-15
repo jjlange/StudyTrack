767d5bd89d5ac5ecc944817dc9cddb8754ae988e
Justin Lange <jlange@tapzie.com>
2020-10-15 23:15:44 +0200

    Initial Commit

---
+++ TrackAndTrace/TrackAndTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackAndTraceApp.swift
+//  TrackAndTrace
+//
+//  Created by Justin Lange on 15/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct TrackAndTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
